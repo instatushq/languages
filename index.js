7c0f4966c69d252b2d7633aef8d9472bf6341877
Ali Salah <ali@instatus.com>
2022-10-21 21:33:32 +0200

    add phrases

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ jsonsInDir.forEach((file, i) => {
     const fileData = fs.readFileSync(path.join('./', file))
     const json = JSON.parse(fileData.toString())
 
-    const key = 'iHaveAnIssueToo' // our text key 
-    let text = 'I have an issue too' // our text to translate here
+    const key = 'changeTheme' // our text key 
+    let text = 'Change theme' // our text to translate here
 
     googleTranslate.translate(text, json.languageCode, function (err, translation) {
       console.log(err)
