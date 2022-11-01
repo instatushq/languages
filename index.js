732f61c2cd9fd3a76cf957656185bfee7e11b3d0
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-11-01 03:17:45 +0200

    Add translations to new main status values

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-const fs = require('fs')
+const { readdirSync } = require('fs')
+const fs = require('fs/promises')
 const path = require('path')
 require('dotenv').config()
 
 var api = process.env.APIKEY
 var googleTranslate = require('google-translate')(api)
 
-const jsonsInDir = fs.readdirSync('./').filter(file => path.extname(file) === '.json')
-count = 1
-jsonsInDir.forEach((file, i) => {
+const jsonsInDir = readdirSync('./').filter(file => path.extname(file) === '.json')
+
+jsonsInDir.forEach(async file => {
   if (file && file !== "list.json" && file.includes("lock") === false && file !== "package.json") {
-    const fileData = fs.readFileSync(path.join('./', file))
+    const fileData = await fs.readFile(path.join('./', file))
     const json = JSON.parse(fileData.toString())
 
-    const key = 'goToHome' // our text key 
-    let text = 'Go to home' // our text to translate here
+    const originals = {
+      "partiallyDegradedService": "Partially Degraded Service",
+      "partialSystemOutage": "Partial System Outage",
+      "majorSystemOutage": "Major System Outage",
+      "experiencingMajorOutage": "Experiencing Major Outage",
+      "experiencingPartialOutage": "Experiencing Partial Outage",
+      "experiencingDegradedPerformance": "Experiencing Degraded Performance",
+      "experiencingMinorOutage": "Experiencing Minor Outage",
+      "experiencingPartiallyDegradedPerformance": "Experiencing Partially Degraded Performance",
+      "systemUnderMaintainence": "System Under Maintenance",
+      "componentExperiencingMajorOutage": "{{component}} Experiencing Major Outage",
+      "componentExperiencingPartialOutage": "{{component}} Experiencing Partial Outage",
+      "componentExperiencingDegradedPerformance": "{{component}} Experiencing Degraded Performance",
+      "componentUnderMaintainence": "{{component}} Under Maintenance",
+    }
 
-    googleTranslate.translate(text, json.languageCode, function (err, translation) {
-      console.log(err)
-      try {
+    try {
+      let count = 1
+
+      for (const [key, text] of Object.entries(originals)) {
+        const translation = await new Promise ((res, rej) => googleTranslate.translate(text, json.languageCode, (err, data) => { err ? rej(err) : res(data) }))
         console.log("No: " + count + " " + json.languageCode + " :>", translation.translatedText)
         json[key] = translation.translatedText
-        count++ 
-        fs.writeFile(file, JSON.stringify(json, null, '  '), 'utf8', function (err) {
-          if (err) throw err
-        })
-      } catch (error) {
-        console.log("Error in " + file)
-        console.log(error)
+        count++
+        await fs.writeFile(file, JSON.stringify(json, null, '  '), 'utf8')
       }
-    })
+    } catch (error) {
+      console.log("Error in " + file)
+      console.log(error)
+    }
   } else {
     console.log("This " + file + " isn't a lang file")
   }
